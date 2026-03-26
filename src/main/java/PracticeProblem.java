6868a81e121f46da1f1784a7f4adc0b54462e7e0
Owais-Khan-ICS3U1-02 <343318663@tdsb.ca>
2026-03-26 09:09:09 -0400

    Implement number and string utility methods
    Added methods to determine if a number is even or odd, check if a number is positive or negative, and pluralize a string.

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,44 @@
+/**
+ * File: Else if
+ * Author: Owais Ali Khan
+ * Date Created: March 26, 2026
+ * Date Last Modified: March 26, 2026
+ */
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
+	public static String evenOrOdd(int num) {
+	    if (num%2==0) {
+	        return "Even";
+	    } else if (num==0) {
+	        return "Zero";
+	    }
+	    return "Odd";
+	}
 	
+	public static String positiveOrNegative(int num) {
+	    if (num>0) {
+	        return "Positive";
+	    } else if (num==0) {
+	        return "Zero";
+	    }
+	    return "Negative";
+	}
+	
+	public static String pluralize(String str) {
+	    str = str.toLowerCase();
+	    if (str.endsWith("ey")) {
+	        return "eys";
+	    } else if (str.endsWith("y")) {
+	        return "ies";
+	    } else if (str.endsWith("ife")) {
+	        return "ives";
+	    }
+	    return "s";
+	}
 
 }
